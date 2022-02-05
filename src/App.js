6ab0e4b4bddce82534cab70c9c22ccf9abd3e3a4
Haley Bubley <haleybubley@haleys-imac.lan>
2022-02-05 13:51:29 -0600

    add react-location

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { Outlet, ReactLocation, Router } from 'react-location';
 import './App.css';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
+const location = new ReactLocation;
 function App() {
+  const routes = [
+    { path: "/", element: <Home /> },
+    { path: "/login", element: <Login /> },
+    { path: "/signup", element: <Signup /> },
+  ]
+
+
   return (
-    <div className="App">
-    
-    </div>
+    <Router location={location} routes={routes}>
+      <div className="App">
+
+
+      </div>
+      <Outlet />
+    </Router>
   );
 }
 
