4b0273e04298bea9708b1dc34949830f6992064d
Haley Bubley <haleybubley@haleys-imac.lan>
2022-02-05 14:57:49 -0600

    registration page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -2,32 +2,45 @@ import React, { useState } from 'react';
 import styles from "./Signup.module.css"
 
 const Signup = () => {
-    const [registrationObj, setRegistrationObj] = useState({ email: "", password: "", confirm_password: "" })
+    const [registrationObj, setRegistrationObj] = useState({ email: "", username: "", password: "", confirm_password: "" });
+    const [alert, setAlert] = useState(null)
     const handleFormInputChange = (e) => {
         setRegistrationObj({ ...registrationObj, [e.target.name]: e.target.value })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(registrationObj.password === registrationObj.confirm_password)
-        console.log(registrationObj)
+        if (registrationObj.password === registrationObj.confirm_password) {
+
+            console.log(registrationObj)
+        } else {
+            setAlert("Passwords must match!")
+            setTimeout(() => setAlert(null), 3000)
+        }
     }
-    return (<form className={styles.signup_form}>
-        <h2>Register</h2>
-        <label>
-            <span>email:</span>
-            <input name="email" value={registrationObj.email} onChange={handleFormInputChange} type="email" />
-        </label>
-        <label>
-            <span>password:</span>
-            <input name="password" value={registrationObj.password} onChange={handleFormInputChange} type="password" />
-        </label>
-        <label>
-            <span>confirm password:</span>
-            <input name="password" value={registrationObj.password} onChange={handleFormInputChange} type="password" />
-        </label>
-        <button className="btn" onClick={handleSubmit}>Register</button>
-    </form>);
+    return (
+        <form className={styles.signup_form}>
+            <h2>Signup</h2>
+            {alert && <p>{alert}</p>}
+            <label>
+                <span>email:</span>
+                <input name="email" value={registrationObj.email} onChange={handleFormInputChange} type="email" />
+            </label>
+            <label>
+                <span>username:</span>
+                <input name="username" value={registrationObj.username} onChange={handleFormInputChange} type="email" />
+            </label>
+            <label>
+                <span>password:</span>
+                <input name="password" value={registrationObj.password} onChange={handleFormInputChange} type="password" />
+            </label>
+            <label>
+                <span>confirm password:</span>
+                <input name="confirm_password" value={registrationObj.confirm_password} onChange={handleFormInputChange} type="password" />
+            </label>
+            <button className="btn" onClick={handleSubmit}>Signup</button>
+        </form>
+    );
 };
 
 export default Signup;
