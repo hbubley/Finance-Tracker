ce0b98aa435e2501c0959e033004e7ff0dee2314
Haley Bubley <haleybubley@haleys-imac.lan>
2022-02-05 14:50:50 -0600

    basic login form complete

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Login.module.css'
 
 const Login = () => {
+    const [loginObj, setLoginObj] = useState({ email: "", password: "" })
+
+    const handleFormInputChange = (e) => {
+        setLoginObj({ ...loginObj, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(loginObj)
+    }
+
     return (
         <form className={styles.login_form}>
             <h2>Login</h2>
             <label>
                 <span>email:</span>
-                <input type="email" />
+                <input name="email" value={loginObj.email} onChange={handleFormInputChange} type="email" />
             </label>
             <label>
                 <span>password:</span>
-                <input type="password" />
+                <input name="password" value={loginObj.password} onChange={handleFormInputChange} type="password" />
             </label>
+            <button className="btn" onClick={handleSubmit}>Login</button>
         </form>
     );
 };
