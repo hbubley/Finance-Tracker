f182a31d3139b7d2add99286f9bb6d65b5f23256
Haley Bubley <haleybubley@haleys-imac.lan>
2022-02-05 14:32:28 -0600

    basic loging form designed

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 import styles from './Login.module.css'
 
 const Login = () => {
-  return <div></div>;
+    return (
+        <form className={styles.login_form}>
+            <h2>Login</h2>
+            <label>
+                <span>email:</span>
+                <input type="email" />
+            </label>
+            <label>
+                <span>password:</span>
+                <input type="password" />
+            </label>
+        </form>
+    );
 };
 
 export default Login;
