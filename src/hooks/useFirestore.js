439974e8506f40420e85a2b3d373830386353db3
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-03-25 16:52:09 -0500

    add basic skeleton

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,21 @@
+import { useReducer, useEffect, useState } from 'react';
+import { auth } from 'firebase/auth'
+
+const initialState = {
+    document: null,
+    pendingState: false,
+    error: null,
+    success: null
+}
+
+const firestoreReducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+export const useFirestore = (collection) => {
+    const [response, dispatch] = useReducer(firestoreReducer, initialState);
+
+
+}
