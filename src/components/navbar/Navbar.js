e88dc44629c872329b693dabb9e9490d17fba635
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-03-25 08:47:25 -0500

    conditionallyu render nav links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,15 +2,32 @@ import React from 'react';
 import { Link } from 'react-location';
 import styles from "./Navbar.module.css";
 import { useLogout } from '../../hooks/useLogout'
+import { useAuthContext } from '../../hooks/useAuthContext';
 const Navbar = () => {
     const { logout } = useLogout();
+    const { user } = useAuthContext();
+
+    const userAuthLinkDisplay = user
+        ? <></>
+        : <>
+            <li><Link to={"/login"}>Login</Link></li>
+            <li><Link to={"/signup"}>Signup</Link></li>
+        </>
+
+    const userLogoutButtonDisplay = user
+        ? <>
+            <li>Hello, {user?.displayName}</li>
+            <li className={styles.logout}>
+                <button className="btn" onClick={logout}>Logout</button>
+            </li>
+        </>
+        : <></>
     return (
         <nav className={styles.navbar}>
             <ul className={styles.navbar_list}>
                 <li className={styles.title}><Link to={"/home"}>Finance Tracker</Link></li>
-                <li><Link to={"/login"}>Login</Link></li>
-                <li><Link to={"/signup"}>Signup</Link></li>
-                <li><button className="btn" onClick={logout}>Logout</button></li>
+                {userAuthLinkDisplay}
+                {userLogoutButtonDisplay}
             </ul>
         </nav>
     );
