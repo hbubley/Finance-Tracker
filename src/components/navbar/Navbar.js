2dad176ffde907122dfd11c762122b3b1192f60f
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-03-25 12:56:57 -0500

    routes protecc routes redriecc and most importantly

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Link } from 'react-location';
+import { Link } from 'react-router-dom';
 import styles from "./Navbar.module.css";
 import { useLogout } from '../../hooks/useLogout'
 import { useAuthContext } from '../../hooks/useAuthContext';
 const Navbar = () => {
     const { logout } = useLogout();
     const { user } = useAuthContext();
-    
-    const userAuthLinkDisplay = user
-        ? <></>
-        : <>
+
+    const userAuthLinkDisplay = !user &&
+        <>
             <li><Link to={"/login"}>Login</Link></li>
             <li><Link to={"/signup"}>Signup</Link></li>
         </>
 
-    const userLogoutButtonDisplay = user
-        ? <>
+    const userLogoutButtonDisplay = user &&
+        <>
             <li>Hello, {user?.displayName}</li>
             <li className={styles.logout}>
                 <button className="btn" onClick={logout}>Logout</button>
             </li>
         </>
-        : <></>
+
+
     return (
         <nav className={styles.navbar}>
             <ul className={styles.navbar_list}>
